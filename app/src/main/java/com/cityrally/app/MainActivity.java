af455ecbe082d3adf8a640d1e9acbed4dc01b6a5
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-11-26 18:17:14 +0100

    mistery fragment

--- app/src/main/java/com/cityrally/app/MainActivity.java
+++ app/src/main/java/com/cityrally/app/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 import com.cityrally.app.manager.Manager;
 import com.cityrally.app.view.ChallengesFragment;
 import com.cityrally.app.view.MapFragment;
+import com.cityrally.app.view.MisteryFragment;
 
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -78,8 +79,11 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
             case 1:
                 fragment = new ChallengesFragment();
                 break;
+            case 2:
+                fragment = new MisteryFragment();
+                break;
             default:
-                fragment = PlaceholderFragment.newInstance(position + 1);
+                fragment = new MapFragment();
                 break;
         }
 
@@ -137,53 +141,4 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private View mView;
-
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-            super();
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            mView = rootView;
-            return rootView;
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
---
+++ app/src/main/java/com/cityrally/app/view/MisteryFragment.java
@@ -0,0 +1,32 @@
+package com.cityrally.app.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.cityrally.app.MainActivity;
+import com.cityrally.app.R;
+
+/**
+ * Created by Pierre-Olivier on 26/11/2014.
+ */
+public class MisteryFragment extends Fragment {
+    public MisteryFragment() {
+        super();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mistery, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(3);
+    }
+}
